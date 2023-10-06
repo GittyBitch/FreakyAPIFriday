8b2adf3feb923b83bd8124a84583ddf33e2707a5
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-10-06 10:24:14 +0200

    add stub for simple unit test

---
+++ test_api.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestAPIDemo(unittest.TestCase):
+    def testStuff(self):
+        self.assertEqual(1,1,"Unit Tests funktionieren nicht")
+
+if __name__ == "__main__":
+    main()
